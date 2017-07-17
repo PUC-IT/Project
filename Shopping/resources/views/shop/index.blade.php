623851f29bab1c84ac90dd2e99a911cf3a974074
choutola <tola@placepass.com>
2017-07-17 16:22:01 +0700

    update

---
+++ Shopping/resources/views/shop/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layout.master')
+
+@section('title')
+	My Shopping Cart
+@endsection
+
+@section('content')
+	<div class="row">
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://www.goldonecomputer.com/image/cache/catalog/products/SSD/M.2/ASX8000NP-128_05-200x200.png" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p>Description of products</p>
+	        <div class="clearfix">
+	        	<div class="pull-left">$ 12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://www.goldonecomputer.com/image/cache/catalog/products/SSD/M.2/ASX8000NP-128_05-200x200.png" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p>Description of products</p>
+	        <div class="clearfix">
+	        	<div class="pull-left">$ 12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://www.goldonecomputer.com/image/cache/catalog/products/SSD/M.2/ASX8000NP-128_05-200x200.png" alt="...">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p>Description of products</p>
+	        <div class="clearfix">
+	        	<div class="pull-left">$ 12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+@endsection
