dbb12fcd9282e75b092fc3715224d9a51406d136
choutola <tola@placepass.com>
2017-07-17 11:02:44 +0700

    laravel Shopping

--- ComputerShop/application/views/about.php
+++ ComputerShop/application/views/about.php
@@ -1,16 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Welcome to about page</title>
 	
 </head>
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>About us</h1>
 
 	
 </div>
